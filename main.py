5db670ded989750cfa3710cd35107ceba12356aa
Sankalp Pandey <82685501+sanpdy@users.noreply.github.com>
2023-11-24 13:06:54 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Activation
+from tensorflow.keras.optimizers import RMSprop
+
+filepath = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
